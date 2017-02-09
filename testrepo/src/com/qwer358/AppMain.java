ed41c2da0d567888851061de32896f51671a5505
qwer358 <qwer358@hotmail.com>
2017-02-09 11:10:22 -0600

    initial commit, java code only plus .files

---
+++ testrepo/src/com/qwer358/AppMain.java
@@ -0,0 +1,15 @@
+package com.qwer358;
+
+public class AppMain {
+
+	public static void main(String[] args) 
+	{
+		        System.out.println( "class AppMain, Hello World!" );
+		        Test1.greeting();
+	}
+
+			public int calculateSomething() {
+				return 0;
+			}
+
+}
---
+++ testrepo/src/com/qwer358/Test1.java
@@ -0,0 +1,7 @@
+package com.qwer358;
+
+public class Test1 {
+	static public void greeting() {
+	System.out.println("class Test1");
+	}
+}
